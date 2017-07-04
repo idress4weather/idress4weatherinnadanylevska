44c7f77fb8fc1f3df55a977a9cbc9b86cf23781b
idress4weatherinnadanylevska <inna.danylevska@gmail.com>
2017-07-04 13:49:52 +0300

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -48,7 +48,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(49.6019599, 34.4171508,11) // New York 40.6700, -73.9400
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
